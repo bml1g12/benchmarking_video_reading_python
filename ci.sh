b166e8987d321e52bdf9fa690316cc2d88935f17
Benjamin Lowe <benjamin@sc0ville.com>
2021-01-11 23:12:17 +0900

    tests: make CI less ram intensive

---
+++ ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -Eeuxo pipefail
+for filename in assets/video_720x480.mkv 
+	do
+		echo "Benchmark for $filename"
+		echo "Running unblocked (no consumer bottleneck) benchmark"
+		python video_reading_benchmarks/main.py --isiolimited --duration 0 --inputvideo $filename
+		echo "Running IO limited benchmark"
+		python video_reading_benchmarks/main.py --isiolimited --inputvideo $filename
+		echo "Running CPU limited benchmark"
+		python video_reading_benchmarks/main.py --inputvideo $filename 
+	done
